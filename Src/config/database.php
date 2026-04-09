e6c9fa2b0c7cb9a4a38dde51acc07442475acfc7
dengcao <8866629@qq.com>
2026-04-09 19:17:51 +0800

    2.0

--- Src/config/database.php
+++ Src/config/database.php
@@ -30,7 +30,7 @@ return [
             // 用户名
             'username'        => env('DB_USER', 'root'),
             // 密码
-            'password'        => env('DB_PASS', 'CICu2raNt4Z4ViwD'),
+            'password'        => env('DB_PASS', 'root'),
             // 端口
             'hostport'        => env('DB_PORT', '3306'),
             // 数据库连接参数
